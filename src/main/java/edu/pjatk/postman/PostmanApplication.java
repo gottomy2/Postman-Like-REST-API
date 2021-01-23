61de42edf0a6a5eb19a78acd902bd22536b1a21f
gottomy2 <igottomy@gmail.com>
2021-01-23 18:57:36 +0100

    added docstring to PostmanApplication

--- src/main/java/edu/pjatk/postman/PostmanApplication.java
+++ src/main/java/edu/pjatk/postman/PostmanApplication.java
@@ -3,6 +3,11 @@ package edu.pjatk.postman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Igor Motowidło (gottomy2)
+ * Main Application. Runs whole project
+ */
+
 @SpringBootApplication
 public class PostmanApplication {
 
